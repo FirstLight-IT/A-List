77aea3c3780e2e960c57f337164f59cb171ddb97
Carl Dominise <janncarlmykodominise@gmail.com>
2026-02-05 22:08:18 +0800

    version 0.2 | Linked Login HTML Page to main.py

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = 'MURPTHECAT123!'
 
-    app.register_blueprint
+    #app.register_blueprint()
 
     return app
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from Website import create_app
+from flask import render_template, request
 
 app = create_app()
+
+@app.route('/')
+def default():
+    return "<h1>WELCOME TO IASite</h1>"
+
+@app.route('/login')
+def login():
+    return render_template('ias_signup.html')
+
 
 if __name__ ==  '__main__':
     app.run(debug=True)   
