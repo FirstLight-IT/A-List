e0577afa0be5cf55bcc0308a8502be218e394879
domi217 <boligao.vince_raymund@hnu.edu.ph>
2026-02-09 16:21:38 +0800

    version 0.12 | Fixed cache bug + added comments

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-db = SQLAlchemy()
+db = SQLAlchemy() #Creates db object (defines models, handles queries, manage sessions)
 login_manager = LoginManager()
 
 def getApp():
     app = Flask(__name__, template_folder='templates')
 
     app.config['SECRET_KEY'] = "TeamFirstLight"
+    #connects the our database to the app - using SQLite and store data in app.db
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -27,5 +28,13 @@ def getApp():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+    
+    @app.after_request
+    def add_no_cache_headers(response):
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, private"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+
 
     return app
